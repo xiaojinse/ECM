be56c036bfc51351e2ed1cb2ae5dad67f1165651
xiaojinse <helen.xiao@ericsson.com>
2016-03-25 15:17:53 +0900

    Update GRE.sh

--- GRE.sh
+++ GRE.sh
@@ -29,20 +29,22 @@ ovs-vsctl add-port mylabovs-gre veth101
 ip link set veth101 up
 ovs-vsctl set port veth101 tag=501
 
+ip netns exec red ping -c 4 10.163.168.222
+
 # on S02:
 
 ovs-vsctl add-br mylabovs-gre
 ovs-vsctl add-port mylabovs-gre gre0 -- set interface gre0 type=gre options:remote_ip=10.168.1.14
 
 ip netns add red
-ip link add veth0 type veth peer name veth1
-ip link set veth0 netns red
-ip netns exec red ip addr add 10.163.168.222/27 dev veth0
-ip netns exec red ip link set veth0 up
-ip netns exec red ip a
+ip link add veth100 type veth peer name veth101
+ip link set veth100 netns red
+ip netns exec red ip addr add 10.163.168.222/27 dev veth100
+ip netns exec red ip link set veth100 up
 
-ovs-vsctl add-port mylabovs-gre veth1
-ip link set veth1 up
-ovs-vsctl set port veth1 tag=501
+ovs-vsctl add-port mylabovs-gre veth101
+ip link set veth101 up
+ovs-vsctl set port veth101 tag=501
 
+ip netns exec red ping -c 4 10.163.168.221
 
